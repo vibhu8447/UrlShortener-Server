006d36509a36202443e28a65748e30db1d8704f7
Vibhu Sharma <vibhusharma@Vibhus-MacBook-Air.local>
2022-07-12 03:46:02 +0530

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const mongoose =  require('mongoose');
 const uuid =  require('uuid');  
 const URLModel =  require('./Model/Url');
+
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
